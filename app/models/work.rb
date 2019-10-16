2c767cf7b78ef0a5455f74b6ba5526f63efca053
Emily Vomacka <emilyvomacka@gmail.com>
2019-10-15 22:02:08 -0700

    completed index views for homepage and works

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,14 @@
 class Work < ApplicationRecord
+
+  def self.spotlight
+    return self.all.sample
+  end 
+
+  def votes
+    return rand(1..10)
+  end 
+
+  def self.top_ten(media)
+    return Work.where(category: "#{media}").select(10)
+  end 
 end
