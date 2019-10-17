93b8400829dd113cc67523b9da5019002b85219e
Emily Vomacka <emilyvomacka@gmail.com>
2019-10-17 14:12:09 -0700

    additional work model tests

--- app/models/work.rb
+++ app/models/work.rb
@@ -5,18 +5,17 @@ class Work < ApplicationRecord
   validates :creator, presence: true
   
   def self.spotlight
+    return nil if self.count == 0
     if self.count > 5
       popular_stuff = self.all.max_by(5) { |work| work.votes.count}
-      popular_stuff = Work.sample(5) if popular_stuff.nil?
     else 
       popular_stuff = self.all.max_by(self.count / 2) { |work| work.votes.count}
-      popular_stuff = Work.sample(self.count / 2) if popular_stuff.nil?
     end 
     return popular_stuff.sort_by { |work| work.votes.last.created_at }.last
   end 
   
-  
   def self.top_ten(media)
+    return nil if self.count == 0
     category_subset = self.where(category: media)
     if category_subset.count > 20
       return category_subset.max_by(10) { |x| x.votes.count}
@@ -26,6 +25,7 @@ class Work < ApplicationRecord
   end 
   
   def self.category_desc_by_vote_count(media)
+    return nil if self.count == 0
     category_subset = self.where(category: media)
     return category_subset.max_by(category_subset.length) { |work| work.votes.count }
   end 
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,25 @@
 require "test_helper"
 
 describe Work do
-  #relations: no relations yet
-  describe "relations" do 
-    it "has many votes" do 
+  let (:new_work) { 
+    Work.new(title: "AMSP", creator: "Radiohead", publication_year: "2016")
+  }
+  
+  it "can be instantiated" do 
+    expect(new_work.valid?).must_equal true
+  end 
+  
+  it "will have the required fields" do
+    new_work.save
+    work = Work.first
+    puts work.title
+    [:category, :title, :creator, :publication_year, :description].each do |field|
+      expect(work).must_respond_to field
+    end 
+  end 
+
+  describe "relationships" do 
+    it "can have many votes" do 
       work = works(:okc)
       expect(work.votes.count).must_equal 2
       expect(work.votes.first).must_be_instance_of Vote
@@ -17,16 +33,16 @@ describe Work do
       expect(work.votes).must_include new_vote
     end 
   end 
-
+  
   describe "validations" do
     before do 
       @work = Work.new(category: "album", title: "a", creator: "b", publication_year: "1990", description: "e")
     end 
-    #validations: all passing
+    
     it "is valid when all fields are present" do
       expect(@work.valid?).must_equal true
     end 
-    #validations: each missing
+    
     it 'is invalid without a title' do
       @work.title = nil
       expect(@work.valid?).must_equal false
@@ -39,41 +55,56 @@ describe Work do
       expect(@work.errors.messages).must_include :creator
     end 
   end 
-  #custom methods: top_ten
-  describe "top_ten method" do
-    it 'returns 10 items when the list of works is greater than 20' do
-      25.times do 
-        work = Work.create(category: "album", title: "a", creator: "b", publication_year: "1990", description: "e")
-      end 
-      top_ten = Work.top_ten("album")
-      expect(top_ten.length).must_equal 10
-    end 
-    
-    it 'returns a list of length Work.count / 2, when Work.count < 10' do
-      top_ten = Work.top_ten("album")
-      expect(top_ten.length).must_equal 2
-    end 
-  end
   
-  describe "category_desc_by_vote_count" do
-    it "returns an ordered list of all works in a given category" do
+  describe "custom methods" do
+    describe "top_ten" do
+      it 'returns 10 items when the list of works is greater than 20' do
+        25.times do 
+          work = Work.create(category: "album", title: "a", creator: "b", publication_year: "1990", description: "e")
+        end 
+        top_ten = Work.top_ten("album")
+        expect(top_ten.length).must_equal 10
+      end 
+      
+      it 'returns a list of length Work.count / 2, when Work.count < 10' do
+        top_ten = Work.top_ten("album")
+        expect(top_ten.length).must_equal 2
+      end 
+      
+      it 'returns a list ordered by vote count' do
+        top_ten = Work.top_ten("album")
+        assert_operator top_ten.first.votes.count, :>, top_ten.last.votes.count
+      end 
+
+      it 'returns works with a tied number of votes in alphabetical order by title' do
+
+      end 
+
+      #how does it handle works with no votes 
+    end
+  end 
+
+  describe 'category_desc_by_vote_count' do
+    it 'returns a list of all works in a given category ordered by vote count' do
       best_albums = Work.category_desc_by_vote_count("album") 
       expect(best_albums.length).must_equal 4
       expect(best_albums[0].title).must_equal "OK Computer"
       expect(best_albums[-1].title).must_equal "Amnesiac"
     end 
+
+    #how does it handle a tie between works
+
+
   end 
-end 
-
-
-# describe "spotlight method" do 
-#   it 'returns a Work object' do
-#     spotlight = Work.spotlight
-#     expect(spotlight).must_be_instance_of Work
-#   end
-
-#   it 'returns the most recently upvoted work in the subset of works with the most votes' do
-#     expect(Work.spotlight).must_equal :httt
-#   end 
-# end 
-
+  
+  # describe "spotlight method" do 
+  #   it 'returns a Work object' do
+  #     spotlight = Work.spotlight
+  #     expect(spotlight).must_be_instance_of Work
+  #   end
+  
+  #   it 'returns the most recently upvoted work in the subset of works with the most votes' do
+  #     expect(Work.spotlight).must_equal :httt
+  #   end 
+  # end 
+ end 
