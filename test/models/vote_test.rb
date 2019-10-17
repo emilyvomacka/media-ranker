f701d61aa10eb8d7b30b72cef1c8dfa6a8bc475b
Emily Vomacka <emilyvomacka@gmail.com>
2019-10-16 23:16:01 -0700

    added vote test

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 describe Vote do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "relations" do
+    before do 
+      @vote = votes(:one)
+    end 
+    it "belongs to a user" do
+      expect(@vote.user).must_be_instance_of User
+    end
+
+    it "belongs to a work" do
+      expect(@vote.work).must_be_instance_of Work
+    end 
+  end 
+
+  describe "validations" do
+    before do 
+      @work = Work.create(category: "album", title: "a", creator: "b", publication_year: "1990", description: "e")
+      @user = User.create(username: "test")
+      @vote = Vote.new(user: @user, work: @work)
+    end 
+
+    it "is valid when all fields are present" do
+      expect(@vote.valid?).must_equal true
+    end 
+  end 
 end
