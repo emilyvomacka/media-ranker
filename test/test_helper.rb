fff4767d0c80e66d13d17bdad88668eebc7a77a4
Emily Vomacka <emilyvomacka@gmail.com>
2019-10-17 09:57:42 -0700

    added test-helper login method for future controller tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,19 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+  
+  def perform_login(user = nil)
+    user ||= User.first
+    
+    login_data = {
+      user: {
+        username: user.username,
+      },
+    }
+    post login_path, params: login_data
+    
+    expect(session[:user_id]).must_equal user.id
+    
+    return user
+  end
 end
