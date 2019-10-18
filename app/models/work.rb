519cd45b7dd23ba777f5d714e787cd5e1077b83c
Emily Vomacka <emilyvomacka@gmail.com>
2019-10-18 13:49:28 -0700

    edited spotlight method to account for zero votes

--- app/models/work.rb
+++ app/models/work.rb
@@ -8,7 +8,9 @@ class Work < ApplicationRecord
   
   def self.spotlight
     return nil if self.count == 0
-    if self.count > 5
+    if self.count > 0 && Vote.count == 0
+      return self.sample(1)
+    elsif self.count > 5
       popular_stuff = self.all.max_by(10) { |x| x.votes.count }
     else 
       popular_stuff = self.all.max_by(self.count / 2) { |x| x.votes.count }
